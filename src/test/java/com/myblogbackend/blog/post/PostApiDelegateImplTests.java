c8c9301945b4aa74d3c378759b99ad3b53cad14c
Tuan Nguyen <tuan@pezzie.com>
2024-03-09 23:45:35 +0200

    Update test cases

--- src/test/java/com/myblogbackend/blog/post/PostApiDelegateImplTests.java
+++ src/test/java/com/myblogbackend/blog/post/PostApiDelegateImplTests.java
@@ -21,36 +21,12 @@ public class PostApiDelegateImplTests {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private CategoryRepository categoryRepository;
-    @MockBean
     private UsersRepository userRepository;
     @MockBean
     private PostRepository postRepository;
 
     @Test
     public void testCreatePost() throws Exception {
-
-//        // Mock the category repository
-//        UUID categoryId = UUID.randomUUID();
-//        CategoryEntity category = new CategoryEntity();
-//        category.setId(categoryId);
-//        Mockito.when(categoryRepository.findById(categoryId)).thenReturn(Optional.of(category));
-//
-//        // Mock the user repository
-//        UserEntity user = new UserEntity();
-//
-//        // Mock the post repository
-//        PostRequest postRequest = new PostRequest();
-//        postRequest.setCategoryId(categoryId);
-//        PostEntity createdPost = new PostEntity();
-//        createdPost.setId(UUID.randomUUID());
-//        Mockito.when(postRepository.save(Mockito.any(PostEntity.class))).thenReturn(createdPost);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/posts")
-//                        .header("Authorization", "Bearer .eyJzdWIiOiJmb29AZW1haWwuY29tIiwiZXhwIjoxNjM4ODU1MzA1LCJpYXQiOjE2Mzg4MTkzMDV9.")
-//                        .content(IntegrationTestUtil.asJsonString(postRequest))
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
 
 
     }
