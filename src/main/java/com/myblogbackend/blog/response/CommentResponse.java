26da473db188b0995f8e58a0251accbff79ccbf8
tuannguyen <tuanquangnguyen1710@gmail.com>
2024-03-27 18:29:29 +0200

    Return created_date

--- src/main/java/com/myblogbackend/blog/response/CommentResponse.java
+++ src/main/java/com/myblogbackend/blog/response/CommentResponse.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,6 +14,7 @@ public class CommentResponse {
     private UUID id;
     private String content;
     private String userName;
+    private Date createdDate;
     private List<CommentResponse> replies = new ArrayList<>();
 
 }
